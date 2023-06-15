9e0aa8ea639fc0da8dc866f2569cc0848dddcd71
mahmoud-bahaa <mahmoud.mohamed.bahaa@gmail.com>
2023-06-15 21:41:42 +0100

    Automatic commit from Bubble

--- elements/ABg-8516e/element_actions/ABm-8516u/run.js
+++
@@ -1,7 +0,0 @@
-function(instance, properties, context) {
-
-
-   instance.data.vjsPlayer.play();
-   instance.publishState("status", "playing");
-
-}
--- elements/ABg-8516e/element_actions/ABn-8516w/run.js
+++
@@ -1,7 +0,0 @@
-function(instance, properties, context) {
-
-
-   instance.data.vjsPlayer.pause();
-    instance.publishState("status", "pause");
-
-}
