0c1eb5bb28e8652647df3344745199756930fc9d
mahmoud-bahaa <mahmoud.mohamed.bahaa@gmail.com>
2023-06-18 22:51:45 +0100

    Automatic commit from Bubble

--- elements/AAC-850m6/element_actions/AAu-850qa/run.js
+++ elements/AAC-850m6/element_actions/AAu-850qa/run.js
@@ -2,7 +2,6 @@ function(instance, properties, context) {
 
 
   let videoTrackList = instance.data.vjsPlayer.videoTracks();
-  let textTrackList =  instance.data.vjsPlayer.textTracks();
   let audioTrackList =  instance.data.vjsPlayer.audioTracks();
     
   
@@ -12,11 +11,6 @@ function(instance, properties, context) {
     	instance.data.vjsPlayer.removeTrack(myVideoTrack);  
   }
   
-  // Loop on textTrackList
-  for (let i = 0; i < textTrackList.length; i++) {
-      	let myTextTrack = textTRackList[i];
-    	instance.data.vjsPlayer.removeTextTrack(myTextTrack); 
-  }
   
   // Loop on audioTrackList
   for (let i = 0; i < audioTrackList.length; i++) {
@@ -24,7 +18,5 @@ function(instance, properties, context) {
     	instance.data.vjsPlayer.removeTrack(myAudioTrack); 
   }
     
-    instance.data.vjsPlayer.removeTextTrack(myTextTrack);
-
 
 }
